462caefb32e0c0d4251697b02031d8bdea110d7d
Jackie Ng <jumpinjackie@gmail.com>
2017-03-01 23:31:19 +1100

    - Fix up current proto files after finally running it through protoc - Generate C# library - Add server skeleton solution

---
+++ proxy_gen_windows.bat
@@ -0,0 +1,7 @@
+@echo off
+SET GRPC_TOOLS=D:\Workspace\vcpkg\installed\x64-windows\tools
+
+echo [grpc]: .net Server/Client
+%GRPC_TOOLS%\protoc.exe -Isrc/Common/proto --csharp_opt=file_extension=.g.cs --csharp_out src/Common/gRpc/csharp --grpc_out src/Common/gRpc/csharp --plugin=protoc-gen-grpc=%GRPC_TOOLS%\grpc_csharp_plugin.exe src\Common\proto\*.proto
+rem %GRPC_TOOLS%\protoc.exe -Isrc/Common/proto --php_out src/Common/gRpcClient/php --grpc_out src/Server/gRpcServer src\Common\proto\FeatureService.proto src\Common\proto\ResourceService.proto src\Common\proto\RenderingService.proto --plugin=protoc-gen-grpc=%GRPC_TOOLS%\grpc_csharp_plugin.exe
+rem %GRPC_TOOLS%\protoc.exe -Isrc/Common/proto --csharp_out src/Common/gRpcClient/csharp --grpc_out src/Server/gRpcServer src\Common\proto\FeatureService.proto src\Common\proto\ResourceService.proto src\Common\proto\RenderingService.proto --plugin=protoc-gen-grpc=%GRPC_TOOLS%\grpc_csharp_plugin.exe
