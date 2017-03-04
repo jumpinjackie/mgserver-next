e58a47c836585585c18841e725d70b734a926e84
Jackie Ng <jumpinjackie@gmail.com>
2017-03-04 18:45:57 +1100

    Move gRpc folder up one level

--- proxy_clean_windows.bat
+++ proxy_clean_windows.bat
@@ -1,14 +1,14 @@
 @echo off
 
 echo [clean]: .net Server/Client
-if exist %CD%\src\Common\gRpc\csharp\bin rd /S /Q %CD%\src\Common\gRpc\csharp\bin
-if exist %CD%\src\Common\gRpc\csharp\obj rd /S /Q %CD%\src\Common\gRpc\csharp\obj
-if exist %CD%\src\Common\gRpc\csharp\*.cs del %CD%\src\Common\gRpc\csharp\*.cs
+if exist %CD%\src\gRpc\csharp\bin rd /S /Q %CD%\src\gRpc\csharp\bin
+if exist %CD%\src\gRpc\csharp\obj rd /S /Q %CD%\src\gRpc\csharp\obj
+if exist %CD%\src\gRpc\csharp\*.cs del %CD%\src\gRpc\csharp\*.cs
 echo [clean]: PHP Client
-if exist %CD%\src\Common\gRpc\php\Osgeo rd /S /Q %CD%\src\Common\gRpc\php\Osgeo
-if exist %CD%\src\Common\gRpc\php\GPBMetadata rd /S /Q %CD%\src\Common\gRpc\php\GPBMetadata
+if exist %CD%\src\gRpc\php\Osgeo rd /S /Q %CD%\src\gRpc\php\Osgeo
+if exist %CD%\src\gRpc\php\GPBMetadata rd /S /Q %CD%\src\gRpc\php\GPBMetadata
 echo [clean]: C++ Server/Client
-if exist %CD%\src\Common\gRpc\cxx\*.pb.cc del %CD%\src\Common\gRpc\cxx\*.pb.cc
-if exist %CD%\src\Common\gRpc\cxx\*.pb.h del %CD%\src\Common\gRpc\cxx\*.pb.h
+if exist %CD%\src\gRpc\cxx\*.pb.cc del %CD%\src\gRpc\cxx\*.pb.cc
+if exist %CD%\src\gRpc\cxx\*.pb.h del %CD%\src\gRpc\cxx\*.pb.h
 echo [clean]: Java Client
-if exist %CD%\src\Common\gRpc\java\osgeo rd /S /Q %CD%\src\Common\gRpc\java\osgeo
+if exist %CD%\src\gRpc\java\osgeo rd /S /Q %CD%\src\gRpc\java\osgeo
