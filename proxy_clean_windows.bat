5dcac1a510130035af0c4f04fc8a7903e5e29575
Jackie Ng <jumpinjackie@gmail.com>
2017-03-04 20:21:45 +1100

    Fix PHP/Java clean steps

--- proxy_clean_windows.bat
+++ proxy_clean_windows.bat
@@ -5,10 +5,10 @@ if exist %CD%\src\gRpc\csharp\bin rd /S /Q %CD%\src\gRpc\csharp\bin
 if exist %CD%\src\gRpc\csharp\obj rd /S /Q %CD%\src\gRpc\csharp\obj
 if exist %CD%\src\gRpc\csharp\*.cs del %CD%\src\gRpc\csharp\*.cs
 echo [clean]: PHP Client
-if exist %CD%\src\gRpc\php\Osgeo rd /S /Q %CD%\src\gRpc\php\Osgeo
+if exist %CD%\src\gRpc\php\OSGeo rd /S /Q %CD%\src\gRpc\php\OSGeo
 if exist %CD%\src\gRpc\php\GPBMetadata rd /S /Q %CD%\src\gRpc\php\GPBMetadata
 echo [clean]: C++ Server/Client
 if exist %CD%\src\gRpc\cxx\*.pb.cc del %CD%\src\gRpc\cxx\*.pb.cc
 if exist %CD%\src\gRpc\cxx\*.pb.h del %CD%\src\gRpc\cxx\*.pb.h
 echo [clean]: Java Client
-if exist %CD%\src\gRpc\java\osgeo rd /S /Q %CD%\src\gRpc\java\osgeo
+if exist %CD%\src\gRpc\java\org rd /S /Q %CD%\src\gRpc\java\org
---
+++ src/gRpc/README.txt
@@ -0,0 +1,4 @@
+This directory contains auto-generated gRpc service stubs and protobuf message classes for various languages.
+
+Code is generated through proxy_gen_windows.bat
+Code can be cleaned through proxy_clean_windows.bat
