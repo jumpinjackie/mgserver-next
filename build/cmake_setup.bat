8b9e069d1a9aaf6e7efb634e50d3169d0e59a914
Jackie Ng <jumpinjackie@gmail.com>
2017-03-26 01:15:15 +1100

    Fix cmake_setup.bat up some more

--- build/cmake_setup.bat
+++ build/cmake_setup.bat
@@ -10,7 +10,7 @@ if "%1" == "Win32" (
     goto setup_config
 )
 if "%1" == "Win64" (
-    set PLAT=%
+    set PLAT=%1
     set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
     goto setup_config
 )
@@ -30,7 +30,9 @@ if "%2" == "Release" (
 :ready
 set BUILD_DIR=%PLAT%_%CONF%
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
-cmake -G %CMAKE_GENERATOR% -B%BUILD_DIR% -DCMAKE_BUILD_TYPE=%CONF% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake ..
+pushd %BUILD_DIR%
+cmake -G %CMAKE_GENERATOR% -B%BUILD_DIR% -DCMAKE_BUILD_TYPE=%CONF% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake ..\..
+popd
 goto done
 
 :error
